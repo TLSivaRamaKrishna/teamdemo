8486050cea1a94a6363c21768acefb493fdfae99
TLSivaRamaKrishna <drsivamca.mtech@gmail.com>
2022-04-30 12:23:53 +0530

    this is demo commit

---
+++ src/main/java/org/kmit/testproject/App.java
@@ -0,0 +1,13 @@
+package org.kmit.testproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello SRK's DevOps World!" );
+    }
+}
